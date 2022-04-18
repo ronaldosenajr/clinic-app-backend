93172117ec8e179539176e5f86acdead552eb619
Ronaldo Junior <denho_sena@hotmail.com>
2022-04-18 15:39:11 -0400

    Criado a Model de Users

---
+++ src/database/models/UsersModel.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public id: number;
+
+  public username: string;
+
+  public role: string;
+
+  public email: string;
+
+  public password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
