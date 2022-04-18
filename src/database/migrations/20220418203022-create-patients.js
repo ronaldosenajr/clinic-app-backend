f343900992a1b059bb73f05f4364d4e55119170b
Ronaldo Junior <denho_sena@hotmail.com>
2022-04-18 17:24:52 -0400

    feat: criacao da migration de patients

---
+++ src/database/migrations/20220418203022-create-patients.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Patients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name_responsible: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      cpf_nf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    }, {
+      underscored: true
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Patients');
+  }
+};
